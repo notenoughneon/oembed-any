d7f8af8b36fb5b8a136c739a5c35e8032752131d
Emma Kuo <emma.c.kuo@gmail.com>
2016-09-07 20:08:12 -0700

    port from skein

---
+++ test.ts
@@ -0,0 +1,31 @@
+import * as assert from 'assert';
+import oembed from './index';
+
+describe('oembed', function() {
+    it('youtube', function() {
+        return oembed('https://www.youtube.com/watch?v=J---aiyznGQ')
+        .then(res => {
+            assert.equal(res.title, 'Charlie Schmidt\'s Keyboard Cat! - THE ORIGINAL!');
+        });
+    });
+
+    it('no oembed link', function() {
+        return oembed('https://google.com')
+        .then(res => {
+            throw new Error('expected an exception');
+        })
+        .catch(err => {
+            assert.equal(err.message, 'No oembed link found');
+        });
+    });
+
+    it('link 404', function() {
+        return oembed('https://google.com/asdfasdfasdfsf')
+        .then(res => {
+            throw new Error('expected an exception');
+        })
+        .catch(err => {
+            assert.equal(err.message, 'https://google.com/asdfasdfasdfsf returned status 404');
+        });
+    });
+});
